4a4a7e3d9ad6ccf0e73673e94a364f9a71ab14e1
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-11-05 17:17:15 -0500

    connected database and added env package

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
+
+const bodyParser = require('body-parser');
+
+// const jobDatabase = [];
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('dist'));
-
-
-app.get('/', function (req, res) {
-   res.sendFile(__dirname +'/dist/index.html')
+MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urtu5cf.mongodb.net/?retryWrites=true&w=majority`, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
   })
+  .catch(error => console.error(error))
 
-app.listen(3000, function () {
-    console.log('listening on 3000')
-  })
+    
+
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname +'/dist/index.html')
+       })
+     
+       app.post('/jobs', (req, res) => {
+        jobDatabase.push(req.body)
+        console.log(jobDatabase)
+       })
+     
+     app.listen(3000, function () {
+         console.log('listening on 3000')
+       })
+  
