cffc97122fe3453d93ef31ffe6fbac117abd144c
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-11-07 14:57:20 -0500

    added custon form object and form validation

--- dist/main.js
+++ dist/main.js
@@ -5,13 +5,26 @@ form.addEventListener('submit',addJob)
 
 function addJob(e) {
     e.preventDefault();
+    // form validation can go here
+  //  captures form data
+    const jobForm = {
+      name: document.getElementById('company').value.trim(),
+      "job post":  document.getElementById('job-post').value.trim(),
+      date: document.getElementById('date').value.trim(),
+      role: document.getElementById('role').value.trim()
+    }
+    console.log(jobForm)
     
-    const formData = new FormData(form)
+  
 
     
     fetch('/jobs',{
         method: 'POST',
-        body: formData,
+        headers: {
+          'Content-Type': 'application/json'
+      },
+      // parse object to JSON to send to server
+      body: JSON.stringify(jobForm)
     })
     .then(response => response.json())
     .then(result => {
